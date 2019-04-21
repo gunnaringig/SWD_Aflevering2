f7e193c991308daa2459e7f97ebb8e591727a337
Gunnar Ingi Geirsson <gunnar.ingi.geirsson@gmail.com>
2019-04-21 16:04:42 +0200

    Added switch case, for choosing operation

--- SWD_Mediator/SWD_Mediator/Program.cs
+++ SWD_Mediator/SWD_Mediator/Program.cs
@@ -5,9 +5,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace SWD_Mediator
 {
+
     class Program
     {
         static void Main(string[] args)
@@ -16,11 +18,38 @@ namespace SWD_Mediator
             Widget2 widget2 = new Widget2();
             ConcreteMediator mediator = new ConcreteMediator(widget1, widget2);
 
-            Console.WriteLine("Client triggers operation A.");
-            widget1.DoA();
+            Console.WriteLine("Choose A or B for widget 1");
 
-            Console.WriteLine("Client triggers operation D.");
-            widget2.DoD();
+            var input = Console.ReadKey();
+
+            switch (input.Key) //Switch on Key enum
+            {
+                case ConsoleKey.A:
+                    widget1.DoA();
+                    //Console.Clear();
+                    break;
+                case ConsoleKey.B:
+                    widget1.DoB();
+                    //Console.Clear();
+                    break;
+            }
+
+
+
+
+            Console.WriteLine("Choose C or D for widget 2");
+            switch (input.Key) //Switch on Key enum
+            {
+                case ConsoleKey.C:
+                    widget2.DoC();
+                    //Console.Clear();
+                    break;
+                case ConsoleKey.D:
+                    widget2.DoD();
+                    //Console.Clear();
+                    break;
+            }
+
         }
     }
 }
